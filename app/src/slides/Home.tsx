0fe88ba98718812daa78d4551b8336c23c688504
rharkor <louis@huort.com>
2023-11-26 21:04:47 +0100

    feat: presentation qr

--- app/src/slides/Home.tsx
+++ app/src/slides/Home.tsx
@@ -1,10 +1,28 @@
+import { Link } from "@nextui-org/react";
+import { QRCodeSVG } from "qrcode.react";
+
 export default function Home() {
   return (
     <>
-      <h1>Home example</h1>
-      <p className="text-center">
-        Press the right arrow key to go to the next slide.
-      </p>
+      <h1 className="text-2xl font-bold">Your presentation title</h1>
+      {import.meta.env.VITE_BASE_URL && (
+        <>
+          <QRCodeSVG
+            value={import.meta.env.VITE_BASE_URL}
+            size={256}
+            bgColor="#ffffff"
+            fgColor="#000000"
+            level="L"
+            includeMargin={true}
+          />
+          <p className="text-center">
+            Join this presentation at <br />
+            <Link href={import.meta.env.VITE_BASE_URL}>
+              {import.meta.env.VITE_BASE_URL}
+            </Link>
+          </p>
+        </>
+      )}
     </>
   );
 }
